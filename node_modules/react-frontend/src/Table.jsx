b1a4de26f3474f85bad414126fe679d78be16285
michaelalbertslo <mwalbert@calpoly.edu>
2025-04-04 12:57:43 -0700

    added deleting

--- node_modules/react-frontend/src/Table.jsx
+++ node_modules/react-frontend/src/Table.jsx
@@ -16,8 +16,13 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
+            <td>{row.name}</td>
+            <td>{row.job}</td>
+            <td>
+                <button onClick={() => props.removeCharacter(index)}>
+                    Delete
+                </button>
+            </td>
         </tr>
       );
      }
@@ -31,10 +36,13 @@ function TableHeader() {
 
   function Table(props) {
     return (
-        <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
+    </table>
     );
 }
 
